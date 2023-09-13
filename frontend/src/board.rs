2aab47bdddc568343e997618142ec1125eefc5e6
cowboy8625 <cowboy8625@yahoo.com>
2023-09-13 07:59:42 -0500

    fixed it up bit

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -18,32 +18,35 @@ impl Component for Board {
         Self { }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
         false
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let slots = ctx.props().slots.clone();
-        let onclick = ctx.props().onclick.clone();
+        let onclick = |id: usize| {
+            let func = ctx.props().onclick.clone();
+            Callback::from(move |_| func.emit(id))
+        };
         let getter = |i: usize| {
             slots[i].map(|i| i.to_string()).unwrap_or_default()
         };
         html! {
 <div class="container">
     <div class="row">
-        <button class="bp" onclick={|_|onclick.emit(0)}>{getter(0)}</button>
-        <button class="bp" onclick={|_|onclick.emit(1)}>{getter(1)}</button>
-        <button class="bp" onclick={|_|onclick.emit(2)}>{getter(2)}</button>
+        <button class="bp" onclick={onclick(0)}>{getter(0)}</button>
+        <button class="bp" onclick={onclick(1)}>{getter(1)}</button>
+        <button class="bp" onclick={onclick(2)}>{getter(2)}</button>
     </div>
     <div class="row">
-        <button class="bp" onclick={|_|onclick.emit(3)}>{getter(3)}</button>
-        <button class="bp" onclick={|_|onclick.emit(4)}>{getter(4)}</button>
-        <button class="bp" onclick={|_|onclick.emit(5)}>{getter(5)}</button>
+        <button class="bp" onclick={onclick(3)}>{getter(3)}</button>
+        <button class="bp" onclick={onclick(4)}>{getter(4)}</button>
+        <button class="bp" onclick={onclick(5)}>{getter(5)}</button>
     </div>
     <div class="row">
-        <button class="bp" onclick={|_|onclick.emit(6)}>{getter(6)}</button>
-        <button class="bp" onclick={|_|onclick.emit(7)}>{getter(7)}</button>
-        <button class="bp" onclick={|_|onclick.emit(8)}>{getter(8)}</button>
+        <button class="bp" onclick={onclick(6)}>{getter(6)}</button>
+        <button class="bp" onclick={onclick(7)}>{getter(7)}</button>
+        <button class="bp" onclick={onclick(8)}>{getter(8)}</button>
     </div>
 </div>
         }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -37,12 +37,6 @@ pub struct App {
     pub current_player: Player,
 }
 
-impl App {
-    fn get_slot(&self, id: usize) -> String {
-        self.slots[id].map(|i| i.to_string()).unwrap_or("".into())
-    }
-}
-
 impl Component for App {
     type Message = Msg;
     type Properties = ();
@@ -62,20 +56,49 @@ impl Component for App {
                 true
             }
             Msg::Reset => {
-                false
+                self.slots = [None; 9];
+                self.current_player = Player::X;
+                true
             }
             _ => false,
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let onclick = |id: usize| {
-            ctx.link().callback(move |_| Msg::Click(id))
-        };
+        let onclick =
+            ctx.link().callback(move |id: usize| Msg::Click(id));
+
+        if !is_win(&self.slots) && self.slots.iter().all(|x| x.is_some()) {
+            return html! {
+                <div class="container">
+                    <span class="message">{"No One Won This Time!"}</span>
+                    <button class="reset-btn" onclick={ctx.link().callback(|_| Msg::Reset)}>{"Reset"}</button>
+                </div>
+            };
+        } else if is_win(&self.slots) {
+            let message = format!("{} win!", self.current_player.swap());
+            return html! {
+                <div class="container">
+                    <span class="message">{message}</span>
+                    <button class="reset-btn" onclick={ctx.link().callback(|_| Msg::Reset)}>{"Reset"}</button>
+                </div>
+            };
+        }
         html! {<Board slots={self.slots.clone()} onclick={onclick} />}
     }
 }
 
 fn main() {
     yew::start_app::<App>();
+}
+
+fn is_win(b: &[Option<Player>]) -> bool {
+    (b[0] == b[1]) && (b[1] == b[2]) && (b[0] != None)
+    || (b[3] == b[4]) && (b[4] == b[5]) && (b[3] != None)
+    || (b[6] == b[7]) && (b[7] == b[8]) && (b[6] != None)
+    || (b[0] == b[3]) && (b[3] == b[6]) && (b[3] != None)
+    || (b[1] == b[4]) && (b[4] == b[7]) && (b[1] != None)
+    || (b[2] == b[5]) && (b[5] == b[8]) && (b[2] != None)
+    || (b[0] == b[4]) && (b[4] == b[8]) && (b[0] != None)
+    || (b[2] == b[4]) && (b[4] == b[6]) && (b[2] != None)
 }
